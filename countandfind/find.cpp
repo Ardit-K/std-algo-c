eb85cc39126df0021b310f6115de9c233a9b3f4e
ardit <ak1935@g.rit.edu>
2024-02-25 16:36:28 -0500

    Added find functionality to check for a number equal to another in a collection.

--- countandfind/find.cpp
+++ countandfind/find.cpp
@@ -12,6 +12,15 @@ int main()
 {
     vector<int> nums{1, 2, 3, 4, 5, 6, 7};
     auto isEven = [](int n){return n%2 == 0;};
+    int num = 8;
+    auto numIt = find(begin(nums), end(nums), num);
+    if(*numIt){
+        cout << num << " is in the collection. "<< endl;
+    }
+    else{
+        cout << num << " is not in the collection. "<< endl;
+    }
+    
     // Finds first even number in collection
     auto it = find_if(begin(nums), end(nums), isEven);
     cout << "First even number in vector: " << *it << endl;
